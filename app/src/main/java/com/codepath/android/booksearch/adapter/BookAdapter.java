f4c77fd4e7af8adf6c2751e6ef1104a8a97c6ccc
Quan Vu <nongdenchet8435848@gmail.com>
2016-10-19 09:11:12 +0700

    complete milestone 2

--- app/src/main/java/com/codepath/android/booksearch/adapter/BookAdapter.java
+++ app/src/main/java/com/codepath/android/booksearch/adapter/BookAdapter.java
@@ -1,6 +1,7 @@
 package com.codepath.android.booksearch.adapter;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -8,6 +9,7 @@ import android.widget.TextView;
 
 import com.codepath.android.booksearch.R;
 import com.codepath.android.booksearch.model.Book;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,30 +25,41 @@ public class BookAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     }
 
     public void setBooks(List<Book> books) {
-        // TODO: Insert your code here
+        mBooks = books;
+        notifyDataSetChanged();
     }
 
     public void addBooks(List<Book> books) {
-        // TODO: Insert your code here
+        int startPosition = books.size();
+        mBooks.addAll(books);
+        notifyItemRangeInserted(startPosition, books.size());
     }
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        // TODO: Insert your code here
-        return null;
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_book, parent, false);
+        return new ViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-        // TODO: Insert your code here
+        ViewHolder viewHolder = (ViewHolder) holder;
+        Book book = mBooks.get(position);
+        viewHolder.tvTitle.setText(book.getTitle());
+        viewHolder.tvAuthor.setText(book.getAuthor());
+        Picasso.with(holder.itemView.getContext())
+                .load(book.getCoverUrl())
+                .placeholder(R.drawable.ic_nocover)
+                .into(viewHolder.ivCover);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mBooks.size();
     }
 
-    public class ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder {
 
         @BindView(R.id.ivCover)
         ImageView ivCover;
@@ -58,6 +71,7 @@ public class BookAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
         TextView tvAuthor;
 
         public ViewHolder(View itemView) {
+            super(itemView);
             ButterKnife.bind(this, itemView);
         }
     }
